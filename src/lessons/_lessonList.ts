b48d8b40bda952a5d1fe4ed360a1b56684569066
Wilco Muit <wilco1999muit@gmail.com>
2025-01-17 19:18:44 +0100

    Added lesson 8.

--- src/lessons/_lessonList.ts
+++ src/lessons/_lessonList.ts
@@ -3,6 +3,7 @@ import lesson4Vocabulary from '@/lessons/lesson4/vocabulary'
 import lesson5Vocabulary from '@/lessons/lesson5/vocabulary'
 import lesson6Vocabulary from '@/lessons/lesson6/vocabulary'
 import lesson7Vocabulary from '@/lessons/lesson7/vocabulary'
+import lesson8Vocabulary from '@/lessons/lesson8/vocabulary'
 
 export default function lessonList() {
   return [
@@ -11,5 +12,6 @@ export default function lessonList() {
     lesson5Vocabulary,
     lesson6Vocabulary,
     lesson7Vocabulary,
+    lesson8Vocabulary
   ]
 }
---
+++ src/lessons/lesson8/vocabulary.ts
@@ -0,0 +1,100 @@
+const questions: Array<QuestionType> = [
+  {
+    question: 'אֶבֶן',
+    answers: ['steen'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'דּוֹר',
+    answers: ['geslacht', 'generatie'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'יְרוּשָׁלַיִם',
+    answers: ['Jeruzalem'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'כֹּה',
+    answers: ['zo', 'aldus'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'לֶחֶם',
+    answers: ['brood', 'strijd'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'מִדְבָּר',
+    answers: ['woestijn', 'wildernis', 'mond'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'מָה',
+    answers: ['wat', 'wat voor een', 'hoe', 'waarom'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'מִי',
+    answers: ['wie', 'iemand'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'מִשְׁפָּט',
+    answers: ['rechtspraak', 'uitspraak', 'vonnis', 'recht', 'gerechtigheid'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'משֶׁה',
+    answers: ['Mozes'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'נָבִיא',
+    answers: ['profeet'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'נַעַר',
+    answers: ['jongen', 'knecht'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'נַעֲרָה',
+    answers: ['meisje', 'dienstmaagd'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'סֵפֶר',
+    answers: ['geschrift', 'brief', 'document', 'boek', 'register'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'פֶּן',
+    answers: ['opdat niet', 'want anders'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'רֶגֶל',
+    answers: ['voet', 'been'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'שֶׁמֶן',
+    answers: ['vet', 'olie'],
+    type: 'woordenschat'
+  },
+  {
+    question: 'תּוֹרָה',
+    answers: ['onderwijzing', 'wet'],
+    type: 'woordenschat'
+  }
+]
+const lesson8Vocabulary: LessonType = {
+  id: 'lesson8-vocabulary',
+  name: 'Les 8 – Woordenschat',
+  type: 'woordenschat',
+  active: false,
+  questions
+}
+export default lesson8Vocabulary
